3cce2b191422484985b1b806d24efc57de570640
Arafat Islam Naim <freelancernaim128992@gmail.com>
2026-03-25 13:47:21 +0600

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,103 @@
 import React from 'react';
+import logo from '../../assets/logo.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
+        <div className='bg-[#001931] text-white mt-10'>
+            <footer className="footer sm:footer-horizontal text-white p-10">
+                <nav>
+                    <h6 className="footer-title">About</h6>
+                    <a className="link link-hover">About Us</a>
+                    <a className="link link-hover">Our Mission</a>
+                    <a className="link link-hover">Blog</a>
+                    <a className="link link-hover">Careers</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">App Categories</h6>
+                    <a className="link link-hover">Games</a>
+                    <a className="link link-hover">Productivity</a>
+                    <a className="link link-hover">Education</a>
+                    <a className="link link-hover">Tools</a>
+                    <a className="link link-hover">Entertainment</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Quick Links</h6>
+                    <a className="link link-hover">Home</a>
+                    <a className="link link-hover">Top Apps</a>
+                    <a className="link link-hover">Trending Apps</a>
+                    <a className="link link-hover">Installation</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Contact & Support</h6>
+                    <a className="link link-hover">Contact Us</a>
+                    <a className="link link-hover">Support Center</a>
+                    <a className="link link-hover">Email: zappify@example.com</a>
+                    <a className="link link-hover">Report an Issue</a>
+                </nav>
+                <form>
+                    <h6 className="footer-title">Newsletter</h6>
+                    <fieldset className="w-80">
+                        <label>Enter your email address</label>
+                        <div className="join">
+                            <input
+                                type="email"
+                                placeholder="enter your email"
+                                className="input input-bordered join-item mt-3" />
+                            <button className="btn btn-primary join-item mt-3">Subscribe</button>
+                        </div>
+                        <p className='mt-4 text-sm text-gray-300'>Subscribe for latest apps & updates.</p>
+                    </fieldset>
+                </form>
+            </footer>
+            <footer className="footer border-base-300 border-t px-10 py-4">
+                <aside className="grid-flow-col items-center">
+                    <img className='h-10' src={logo} alt="Logo" />
+                    <p>
+                        <span class="text-gray-300 text-3xl font-bold">
+                                Zappify
+                            </span>
+                        <br />
+                        <small>We provide safe, fast, and verified apps for everyone since 2026.</small>
+                    </p>
+                </aside>
+                <nav className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                        <a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                className="fill-current">
+                                <path
+                                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                            </svg>
+                        </a>
+                        <a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                className="fill-current">
+                                <path
+                                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                            </svg>
+                        </a>
+                        <a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                className="fill-current">
+                                <path
+                                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </nav>
+            </footer>
         </div>
     );
 };
